d11bf96fb4711437a39cc62dcc5dc1da1f587c72
kimya-h <k.haj@danesh.ai>
2023-07-15 11:54:01 +0330

    write some code for try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
+import React from "react";
+import Product from "./components/Product/Product";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    products: [
+      {id :1, title: 'book1', price: 99},
+      {id :2, title: 'book2', price: 99},
+      {id :3, title: 'book3', price: 99},
+    ],
+    showProducts: false,
+  }
+
+
+  changeTitleHandler = (event, id) => {
+    const productIndex = this.state.products.findIndex((item) => {
+      return item.id === id
+
+    })
+
+    const product = {
+      ...this.state.products[productIndex]
+    }
+
+    product.title = event.target.value
+
+    const products = [...this.state.products]
+    products[productIndex] = product
+
+    this.setState({ products : products})
+  }
+
+  toggleProductHandler= () => {
+   const show = this.state.showProducts
+   this.setState({ showProducts: !show })
+  }
+
+  deleteProductHandler = (productIndex) => {
+    const products = [...this.state.products]
+    products.splice(productIndex, 1)
+    this.setState({ products : products})
+  }
+
+
+
+  render() {
+    const btn = {
+      backgroundColor: '#7b1fa2',
+      color : '#ffffff',
+      font : 'inherit',
+      border : 'none',
+      outline : 'none',
+      borderRadius: '3px',
+      padding: '0.6rem',
+      margin: '0.6rem auto',
+    }
+
+
+    let products = null;
+    if (this.state.showProducts) {
+      products = (
+          <div>
+            {
+              this.state.products.map((item, index) => {
+                return <Product 
+                click = {() => this.deleteProductHandler(index)}
+                title = {item.title}
+                price = {item.price} 
+                key = {item.id}
+                change = {(event) => 
+                  this.changeTitleHandler(event, item.id)}
+                />
+              })
+            }
+          </div> 
+      )
+    }
+
+
+
+
+    return (
+      <div className="center">
+          <h2>Book Store</h2>
+          <button style={btn}  onClick={this.toggleProductHandler}>
+             Show/Hide Products </button> 
+          
+            {products}            
+      </div>
+  )
+  }
 }
 
-export default App;
+
+  export default App;
